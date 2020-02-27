56553f6c9fe813470958a1ae2fa7a0f1a52e3098
Luca <luca-marconi@hotmail.com>
2020-02-27 15:57:22 +0100

    completamento esercizio

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.header-right > a').click(function () {
+    $('.hamburger-menu').addClass('active');
+});
+
+
+$('.close').click(function () {
+    $('.hamburger-menu').removeClass('active');
+});
